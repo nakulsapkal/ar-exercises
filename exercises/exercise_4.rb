c8f576387a909dedcba828a9fc2414e944bd5d92
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-20 22:17:24 +0000

    Create data and Fetch data Using Where Method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,25 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey",annual_revenue: 224000, mens_apparel: false,
+womens_apparel: true);
+
+Store.create(name: "Whistler",annual_revenue: 1900000, mens_apparel: true,
+womens_apparel: false);
+
+Store.create(name: "Yaletown",annual_revenue: 430000, mens_apparel: true,
+womens_apparel: true);
+
+
+@mens_stores = Store.where(mens_apparel:true)
+
+	for store in @mens_stores
+		puts "Details: name:#{store.name} & annual_revenue:#{store.annual_revenue}"
+	end
+
+	@womens_stores = Store.where(:womens_apparel => true).where("annual_revenue < ?",1000000)
+
+	for store in @womens_stores
+		puts "Details: name:#{store.name} & annual_revenue:#{store.annual_revenue}"
+	end
