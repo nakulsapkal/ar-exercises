fd10ba80611d3f013c7ae71a332ba38d5f7d331d
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-07-20 21:35:01 +0000

    refactored and removed the variables used for data create code

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,14 +5,13 @@ puts "----------"
 
 # Your code goes below here ...
 
-#store = Store.new
-store=Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true,
+Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true,
 womens_apparel: true);
 
-store=Store.create(name: "Richmond",annual_revenue: 1260000, mens_apparel: false,
+Store.create(name: "Richmond",annual_revenue: 1260000, mens_apparel: false,
 womens_apparel: true);
 
-store=Store.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true,
+Store.create(name: "Gastown",annual_revenue: 190000, mens_apparel: true,
 womens_apparel: false);
 
 puts Store.count(:name)
